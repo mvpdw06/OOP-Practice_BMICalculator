98e72892456c71e45ce81cb9e5f86779ea4f7d43
mvpdw06 <mvpdw06@gmail.com>
2016-06-07 16:58:34 +0800

    using only class concept.

---
+++ CalculateMBI_OOP/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculateMBI_OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            Human human = new Human();
+            human.Height = Convert.ToDouble(this.textBox1.Text);
+            human.Weight = Convert.ToDouble(this.textBox2.Text);
+
+            var BMI = human.BMI;
+            MessageBox.Show("BMI：" + BMI + "，結果為：" + human.Comment);
+        }
+    }
+}
---
+++ CalculateMBI_OOP/Human.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CalculateMBI_OOP
+{
+    public class Human
+    {
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public Gender Gender { get; set; }
+        public double BMI
+        {
+            get { return GetBMI(); }
+        }
+        public string Comment
+        {
+            get { return GetComment(); }
+        }
+
+        private double GetBMI()
+        {
+            return Weight / Math.Pow(Height, 2);
+        }
+
+        private string GetComment()
+        {
+            if (Gender == Gender.Male)
+            {
+                if (BMI < 20)
+                {
+                    return "太瘦";
+                }
+                else if (BMI > 25)
+                {
+                    return "太胖";
+                }
+                else
+                {
+                    return "適中";
+                }
+            }
+            else
+            {
+                if (BMI < 18)
+                {
+                    return "太瘦";
+                }
+                else if (BMI > 22)
+                {
+                    return "太胖";
+                }
+                else
+                {
+                    return "適中";
+                }
+            }
+        }
+    }
+
+    public enum Gender
+    {
+        Male = 0,
+        FeMale = 1
+    }
+}
